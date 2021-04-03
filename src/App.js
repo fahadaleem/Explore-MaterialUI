45875248d6b870b7cd56897210300b4523d2bfb3
Fahad Aleem <faleem396@gmail.com>
2021-04-03 23:49:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
   componentDidMount(){
     this.setState({
       isLogin:window.localStorage.getItem("isLogin"),
-      employeeData:JSON.parse(window.localStorage.getItem("employeeData"))
+      employeeData:employeeData:JSON.parse(window.localStorage.getItem("employeeData"))?JSON.parse(window.localStorage.getItem("employeeData")):[]
     })
 
 
